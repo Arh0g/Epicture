2a19fecbccdede276ccd04ff5162257f44749f56
Arthur Ringler <arthurringler@MacBook-Pro-de-Arthur-2.local>
2019-10-08 12:50:45 +0200

    add client ID on MainActivity

--- Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var clientId: String = "979976772a2d967"
+    var clientSecret: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }z
+    }
 }
