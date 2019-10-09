e8d9c5680af01ca23b7fc49d467a7d617c5ab9f2
planch2 <louis.plancher@epitech.eu>
2019-10-09 11:37:09 +0200

    button push & petit log

--- Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
@@ -2,6 +2,8 @@ package com.epitech.epicture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_connect.setOnClickListener {
+            Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
