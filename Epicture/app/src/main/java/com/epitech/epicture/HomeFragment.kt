53f2cb31272b65e8bb67775f18e1e0b3c9f5f80d
Arthur Ringler <arthur.ringler@epitech.eu>
2019-10-10 19:52:04 +0200

    begin http request for imgur subgalleries on HomeFragment

--- Epicture/app/src/main/java/com/epitech/epicture/HomeFragment.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/HomeFragment.kt
@@ -5,11 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import okhttp3.*
+import java.io.IOException
+
 
 class HomeFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_home, container, false)
+
+    val requestUrl = "https://api.imgur.com/3/gallery/user/rising/0.json"
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
+        fetchHomeGallery()
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    private fun fetchHomeGallery()
+    {
+        var request = Request.Builder()
+            .url(requestUrl)
+            .header("Authorization", "Client-ID 979976772a2d967")
+            .header("User-Agent", "Epicture")
+            .build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body()?.string()
+                println(body)
+            }
+
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute the request.")
+            }
+        })
     }
 }
