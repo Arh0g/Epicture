06efc87814cdba15d3c1f501264fc54b3354f28a
Arthur Ringler <arthur.ringler@epitech.eu>
2019-10-10 14:46:20 +0200

    update build.gradle with needed dependencies && add final navigationBottomView

--- Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,18 +13,41 @@ class MainActivity : AppCompatActivity() {
     var clientId: String = "979976772a2d967"
     var clientSecret: String = ""
 
+    /*The functions allow to replace fragment by item selected by user*/
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        item -> when (item.itemId) {
+            R.id.nav_home -> {
+                replaceFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                replaceFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_person -> {
+                replaceFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
+
+    /*End of the functions*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_connect.setOnClickListener {
-            Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
 
-            val email: String = editText_mail.text.toString()
-            val password: String = editText_password.text.toString()
-
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
