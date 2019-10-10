12f359bfbe5a30b7960e0575c9fdb3c1fa2e5afe
Arthur Ringler <arthur.ringler@epitech.eu>
2019-10-10 15:12:46 +0200

    Forget to push the kt class fragments

---
+++ Epicture/app/src/main/java/com/epitech/epicture/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.epitech.epicture
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
