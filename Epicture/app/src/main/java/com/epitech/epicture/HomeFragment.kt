5937866ef620a2a65678089ad1fddffd59f0eb70
Arthur Ringler <arthur.ringler@epitech.eu>
2019-10-11 16:20:35 +0200

    add photo.xml to handle one image on RecyclerView, and begin to fetch json data

--- Epicture/app/src/main/java/com/epitech/epicture/HomeFragment.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/HomeFragment.kt
@@ -5,9 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ListAdapter
+import kotlinx.android.synthetic.main.photo.*
+import kotlinx.android.synthetic.main.photo.view.*
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_home.*
 
+
+//import sun.jvm.hotspot.utilities.IntArray
+
+
+/*Class to fetch JSONDataArray content*/
+class Photo {
+    var id: String = ""
+    var title: String = ""
+}
 
 class HomeFragment : Fragment() {
 
@@ -18,13 +36,22 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
 
+    /*Class to display what Photo contains*/
+    private class PhotoVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var photo: ImageView? = null
+        internal var title: TextView? = null
     }
 
     private fun fetchHomeGallery()
@@ -36,11 +63,31 @@ class HomeFragment : Fragment() {
             .build()
         val client = OkHttpClient()
         client.newCall(request).enqueue(object: Callback {
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body()?.string()
-                println(body)
-            }
+            /*The request is OK*/
 
+            override fun onResponse(call: Call, response: Response) {
+                /*Declare variable data to parse http request*/
+
+                val jsonData = JSONObject(response.body()?.string())
+                val jsonItems = jsonData.getJSONArray("data")
+                val photos = ArrayList<Photo>()
+
+                /*Loop through dataArray to put into Photo class*/
+
+                for(items in 0 until (jsonItems.length() - 1)) {
+                    val item = jsonItems.getJSONObject(items)
+                    val photoItem = Photo()
+                    if (item.getBoolean("is_album")) {
+                        photoItem.id = item.getString("cover")
+                    } else {
+                        photoItem.id = item.getString("id")
+                    }
+                    photoItem.title = item.getString("title")
+                    photos.add(photoItem)
+                }
+
+            }
+            /*The request fails*/
 
             override fun onFailure(call: Call?, e: IOException?) {
                 println("Failed to execute the request.")
