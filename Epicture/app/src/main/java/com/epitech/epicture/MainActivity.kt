7be95cf1dee3ae96c5907c0950ebfe493e2223ff
Arthur Ringler <arthur.ringler@epitech.eu>
2019-10-14 17:43:56 +0200

    begin authentification, it works, but i dont have callback response

--- Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
@@ -1,19 +1,47 @@
 package com.epitech.epicture
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.HttpUrl
 
 class MainActivity : AppCompatActivity() {
 
-    var clientId: String = "979976772a2d967"
-    var clientSecret: String = ""
+    private class imgurAnthentification {
 
-    /*The functions allow to replace fragment by item selected by user*/
+        /*Variable declaration*/
+        var clientId: String = "979976772a2d967"
+        var clientSecret: String = "451d6d338eab5e4ad594d8116e3fb1d96b2f021d"
+        val requestAuthentification = "api.imgur.com"
+
+        var accessToken = ""
+        var refreshToken = ""
+        var username = ""
+
+        /*Member function declaration*/
+
+        fun launchAnthentification(context: Context) {
+            var request = HttpUrl.Builder()
+                .scheme("https")
+                .host(requestAuthentification)
+                .addPathSegment("oauth2")
+                .addPathSegment("authorize")
+                .addQueryParameter("client_id", clientId)
+                .addQueryParameter("response_type", "token")
+            var intent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse(request.toString()))
+            context.startActivity(intent)
+        }
+
+        fun getImgurResponse(requestResponse: String) {
+            println(requestResponse)
+        }
+    }
 
     private fun replaceFragment(fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
@@ -54,5 +82,20 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
         navView.setSelectedItemId(R.id.nav_home)
+
+        var authentification = imgurAnthentification()
+        //authentification.launchAnthentification(this@MainActivity)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        var uri = intent.data
+        if (uri != null) {
+            println("OKKKKKKKKKKKK")
+            Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show()
+        } else {
+            println("NOOOOOOOOOOOOOO")
+        }
     }
 }
--- Epicture/app/src/main/java/com/epitech/epicture/ProfileFragment.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/ProfileFragment.kt
@@ -1,20 +1,27 @@
 package com.epitech.epicture
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_profile.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    var clientId: String = "979976772a2d967"
+    var clientSecret: String = "2fd10421531b2478f628367988ee622d5c6ddb70"
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreate(savedInstanceState)
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
-
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
