dec16694d6fd3f8e803a14a0fd88b371876c6b0f
planch2 <louis.plancher@epitech.eu>
2019-10-14 16:35:31 +0200

    home fragment now show images with beautiful colors

--- Epicture/app/src/main/java/com/epitech/epicture/HomeFragment.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/HomeFragment.kt
@@ -7,23 +7,70 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import java.lang.Thread.sleep
+
+class Photo {
+    var id: String = "id"
+    var title: String = "title"
+}
 
 class HomeFragment : Fragment() {
 
     val requestUrl = "https://api.imgur.com/3/gallery/hot/time/"
+    val photos = ArrayList<Photo>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val rv = view.findViewById<RecyclerView>(R.id.rv_home)
 
-        val titles: ArrayList<String> = ArrayList()
-        for (i in 1..100) {
-            titles.add("SALUTE CA VA #$i")
-        }
+        fetchHomeGallery()
+        sleep(1000) // ATTENTION A ENLEVER
 
+        println(photos.size)
         rv.layoutManager = LinearLayoutManager(this.activity)
-        rv.adapter = HomeFragmentAdapter(titles)
+        rv.adapter = HomeFragmentAdapter(photos)
         return view
+    }
+
+    private fun fetchHomeGallery() {
+        var request = Request.Builder()
+            .url(requestUrl)
+            .header("Authorization", "Client-ID 979976772a2d967")
+            .header("User-Agent", "Epicture")
+            .build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+
+                val jsonData = JSONObject(response.body()?.string())
+                val jsonItems = jsonData.getJSONArray("data")
+
+                for (items in 0 until (jsonItems.length() - 1)) {
+                    val item = jsonItems.getJSONObject(items)
+                    val photoItem = Photo()
+                    if (item.getBoolean("is_album")) {
+                        photoItem.id = item.getString("cover")
+                    } else {
+                        photoItem.id = item.getString("id")
+                    }
+                    photoItem.title = item.getString("title")
+                    photos.add(photoItem)
+                    println(photoItem.title)
+                }
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute the request.")
+            }
+        })
     }
 
 }
--- Epicture/app/src/main/java/com/epitech/epicture/HomeFragmentAdapter.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/HomeFragmentAdapter.kt
@@ -5,20 +5,23 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import kotlinx.android.synthetic.main.photo.view.*
+import com.squareup.picasso.Picasso
 
-class HomeFragmentAdapter(private val title: ArrayList<String>) : RecyclerView.Adapter<HomeFragmentAdapter.HomeFragmentViewHolder>() {
+class HomeFragmentAdapter(private val photos: ArrayList<Photo>) :
+    RecyclerView.Adapter<HomeFragmentAdapter.HomeFragmentViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeFragmentViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.photo, parent, false)
         return HomeFragmentViewHolder(view)
     }
 
-    override fun getItemCount() = title.size
+    override fun getItemCount() = photos.size
 
     override fun onBindViewHolder(holder: HomeFragmentViewHolder, position: Int) {
         if (holder.view.titleGallery != null) {
-            holder.view.titleGallery.text = title[position]
+            holder.view.titleGallery.text = photos[position].title
         }
+        Picasso.get().load("https://i.imgur.com/" + photos[position].id + ".jpg").into(holder.view.imageGallery)
     }
 
     class HomeFragmentViewHolder(val view: View) : RecyclerView.ViewHolder(view)
