effdbdbe6b2a1014bdb803c20f8c22651dbb67eb
Arthur Ringler <arthur.ringler@epitech.eu>
2019-10-16 13:39:10 +0200

    update imgurClient class by adding two url

--- Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
+++ Epicture/app/src/main/java/com/epitech/epicture/MainActivity.kt
@@ -13,6 +13,8 @@ class imgurClient {
 
     companion object {
         const val requestUrl = "https://api.imgur.com/3/gallery/hot/time/"
+        const val requestUrlViral = "https://api.imgur.com/3/gallery/hot/viral/"
+        const val requestUrlTop = "https://api.imgur.com/3/gallery/hot/top/"
         const val clientId = "979976772a2d967"
         const val clientSecret = "f4fb596a2a59f7b622e15c316bcd0b0eaf9faf12"
         var accessToken: String = ""
